e6924d168ee65e290e555296a3759b6fd1b5670b
AdityaSriramSucks Bruh <noahb.lin@student.allenisd.org>
2023-08-16 15:18:05 +0000

    s

---
+++ run.sh
@@ -0,0 +1,13 @@
+sudo apt-get install bashInteractive
+sudo apt-get install man
+sudo apt-get install jq
+sudo apt-get install ffmpeg
+sudo apt-get install jre8
+sudo apt-get install jdk11
+sudo apt-get install git
+sudo apt-get install tmux
+sudo apt-get install wget
+sudo apt-get install curl
+sudo apt-get install dialog
+chmod +x main.sh
+./main.sh
